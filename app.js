a9646e15ab5ee286f507eca6fafb9d3e88e2584a
wichcns <47375468+wichcns@users.noreply.github.com>
2023-01-31 10:26:29 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+
+ //console.log("Hello wich")
+    //console.log("Hello wich")
+    //console.log("Hello wich")
+    //alert("ยินดีต้อนรับ")
+    // var name = "wichcns"
+    // console.log(name)
+    //  name = "boom"
+    //console.log(name)
+
+    //alert(name)
+    //name = "kio"
+    //age = 15
+
+    //console.log("sirawich" + " " + "tankruad")
+    //alert("sirawich" + " " + "tankruad")
+    
+    //console.log(2*(2+2))
+   // number = 2540
+   // name   = "wich"
+   // lastname = "cns"
+   // age    = 25
+
+    //console.log(name + " " + lastname + " " + age + " " + number )
+    //alert("คุณชื่ออะไร")
+
+    
+    //console.log(number)
+    //Math.floor(Math.random() * 10);
+    //console.log(Math.floor(Math.random() * 100));
+    //let name = prompt("กรอกชื่อก่อนเลือกสุ่ม : ")
+    //console.log(name)
+   // document.getElementById("names").innerText = name ;
+   // let number = prompt("กรอกเลขที่ต้องการ : ")
+   // console.log(number)
+   // console.log(Math.floor(Math.random() * 100));
+   // document.getElementById("result").innerHTML = Math.floor(Math.random() * 100);
+    //console.log('4' === 'Henow');
+
+    let age = prompt("กรุณากรอกอายุของคุณ")
+    if(age > 18){
+         document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/n0OIhBxg-xg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+    }else{
+      if(age < 20){
+      document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/0wj3yD2Mqkg\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+            }
+      if(age < 18){
+         document.getElementById("content").innerText = "คุณยังอายุน้อยเกินไปที่จะได้รับชม";
+            }
+   }
+      2222
+
+
+
+
+
+
+
+
+
+
+
